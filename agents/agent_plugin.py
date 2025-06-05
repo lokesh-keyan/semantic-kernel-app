9fa3ece98ec9eee856d4b3b376e1247e41b569b9
Lokesh Keyan <lokeshkeyanuf@gmail.com>
2025-06-05 14:32:15 -0400

    Function invocation and calling modes

--- agents/agent_plugin.py
+++ agents/agent_plugin.py
@@ -3,6 +3,14 @@ from typing import Annotated, List, Tuple
 from semantic_kernel.connectors.ai.open_ai import AzureChatCompletion
 from semantic_kernel.agents import ChatCompletionAgent
 from semantic_kernel.functions import kernel_function
+from typing import Awaitable, Callable
+from semantic_kernel.kernel import Kernel
+from semantic_kernel.filters import FilterTypes, FunctionInvocationContext
+from semantic_kernel.connectors.ai.function_choice_behavior import FunctionChoiceBehavior
+from semantic_kernel.connectors.ai.open_ai import OpenAIChatPromptExecutionSettings
+from semantic_kernel.functions import KernelArguments
+# older versions
+# from semantic_kernel.kernel_arguments import KernelArguments
 
 class NotesPlugin:
     """A simple plugin to manage notes."""
@@ -21,17 +29,43 @@ class NotesPlugin:
         timestamp = datetime.now().isoformat()
         self.notes.append((timestamp, note))
         return f"Note saved at {timestamp}."
+    
+async def function_invocation_filter(
+    context: FunctionInvocationContext,
+    next: Callable[[FunctionInvocationContext], Awaitable[None]],
+) -> None:
+    # this runs before the function is called
+    print(f"  ---> Calling Plugin {context.function.plugin_name}.{context.function.name} with arguments `{context.arguments}`")
+    # let's await the function call
+    await next(context)
+    # this runs after our functions has been called
+    print(f"  ---> Plugin response from [{context.function.plugin_name}.{context.function.name} is `{context.result}`")
 
 async def notes_agent_example():
     """
     Example of an agent using a plugin to manage notes.
     """
+
+    kernel = Kernel()
+    kernel.add_filter(FilterTypes.FUNCTION_INVOCATION, function_invocation_filter)
+
+    # Auto-invoke (default behavior)
+    # Function Calling Modes
+    # For now, we didn't really mess with the function calling behavior, but Semantic Kernel supports different modes for function calling:
+
+    # Auto: The agent automatically decides whether to call functions based on the context.
+    # RequireFunction: The agent must call at least one function in its response.
+    # Disabled: The agent cannot call functions; it must respond using only text.
+    settings = OpenAIChatPromptExecutionSettings(function_choice_behavior=FunctionChoiceBehavior.Auto())
+
     # Initialize the agent with AzureChatCompletion and the NotesPlugin
     agent = ChatCompletionAgent(
         service=AzureChatCompletion(),
+        kernel=kernel,
         name="notes_assistant",
         instructions="You are a helpful assistant.",
         plugins=[NotesPlugin()],
+        arguments=KernelArguments(settings),
     )
 
     messages = [
